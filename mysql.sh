0fedfc303c3a3f070e596cd7c408c61fa7caf775
manimohan <manimohan.t22@gmail.com>
2024-12-25 13:03:28 +0530

    mysql.sh update

--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 USERID=$(id -u)
 TIME_STAMP=$(date +%F-%H:%M:%S)
-SCRIPT_NAME=$(echo $0)
+SCRIPT_NAME=$(echo $0 |cut -d "." -f1)
 LOG_FILE=/tmp/$SCRIPT_NAME-$TIME_STAMP
 R="\e[31m"
 G="\e[32m"
@@ -15,13 +15,14 @@ then
     echo -e "$R Please Run this Script as super User $N"
     exit 1
     else
-        echo -e "\e[35m Running the Script as super User $N"
+        echo -e "$P Running the Script as super User $N"
 fi
 
 VALIDATE(){
     if [ $1 -ne 0 ] #if $?-previous exit status is not zero, then installation will show failure else success
     then
         echo -e "$2.....$R FAILURE $N"
+        exit 1
     else
         echo -e "$2.....$G SUCCESS $N"
     fi
