10ea44ac80b09c28d093650c75ccfeacda78c51c
manimohan <manimohan.t22@gmail.com>
2024-12-26 21:09:13 +0530

    frontend.sh created

---
+++ frontend.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIME_STAMP=$(date +%F-%H:%M:%S)
+SCRIPT_NAME=$(echo $0 |cut -d "." -f1)
+LOG_FILE=/tmp/$SCRIPT_NAME-$TIME_STAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+P="\e[35m"
+echo "Script Start-Time is:: $TIME_STAMP"
+
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R Please Run this Script as super User $N"
+    exit 1
+    else
+        echo -e "$P Running the Script as super User $N"
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ] #if $?-previous exit status is not zero, then installation will show failure else success
+    then
+        echo -e "$2.....$R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2.....$G SUCCESS $N"
+    fi
+}
+
+dnf install nginx -y &>>$LOG_FILE
+VALIDATE $? "Installing Nginx"
+
+systemctl enable nginx &>>$LOG_FILE
+VALIDATE $? "Enabling Nginx"
+
+systemctl start nginx &>>$LOG_FILE
+VALIDATE $? "Starting  Nginx"
+
+rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
+VALIDATE $? "Removing default /usr/share/nginx/html/* content"
+
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOG_FILE
+VALIDATE $? "Downloading Frontend Content"
+
+cd /usr/share/nginx/html
+rm -rf /tmp/* &>>$LOG_FILE
+unzip /tmp/frontend.zip &>>$LOG_FILE
+VALIDATE $? "Extracting Content to /tmp"
+
+cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf &>>$LOG_FILE
+VALIDATE $? "Copying expense.conf to /etc/nginx/default.d/"
+
+
+systemctl restart nginx &>>$LOG_FILE
+
+
+
+
+
