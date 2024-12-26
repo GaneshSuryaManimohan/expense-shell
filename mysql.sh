f0052a397a63dcc4d3409265caa731c854f4ed6b
manimohan <manimohan.t22@gmail.com>
2024-12-26 19:51:27 +0530

    backend.sh created

--- mysql.sh
+++ mysql.sh
@@ -2,7 +2,7 @@
 USERID=$(id -u)
 TIME_STAMP=$(date +%F-%H:%M:%S)
 SCRIPT_NAME=$(echo $0 |cut -d "." -f1)
-LOG_FILE=/tmp/$SCRIPT_NAME-$TIME_STAMP
+LOG_FILE=/tmp/$SCRIPT_NAME-$TIME_STAMP.log
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
