ddfece8c7128e629bbd46e86b461917c49ad932f
manimohan <manimohan.t22@gmail.com>
2024-12-26 20:40:51 +0530

    backend.sh and backend.service created

--- backend.sh
+++ backend.sh
@@ -10,6 +10,8 @@ Y="\e[33m"
 N="\e[0m"
 P="\e[35m"
 echo "Script Start-Time is:: $TIME_STAMP"
+echo "Please enter DB Password::"
+read -s mysql_root_password
 
 if [ $USERID -ne 0 ]
 then
@@ -46,3 +48,44 @@ then
 else
     echo -e "expense user already exists.....$Y SKIPPING $N"
 fi
+
+mkdir -p /app
+VALIDATE $? "creating /app directory"
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip
+VALIDATE $? "Downloading Content for Backend"
+
+cd /app
+unzip /tmp/backend.zip
+VALIDATE $? "Unzip backend code"
+
+npm install
+VALIDATE $? "Downloading noejs dependencies"
+
+cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service
+VALIDATE $? "copied backend.service to /etc/systemd/system"
+
+systemctl daemon-reload
+VALIDATE $? "Daemon Reload"
+
+systemctl start backend
+VALIDATE $? "Starting  Backend"
+
+systemctl enable backend
+VALIDATE $? "Enabling  Backend service"
+
+dnf install mysql -y
+VALIDATE $? "Installing MySQL Client"
+
+mysql -h db.surya-devops.online -uroot -p${mysql_root_password} < /app/schema/backend.sql
+VALIDATE $? "Loading backend.sql schema"
+
+systemctl restart backend
+VALIDATE $? "Restarting Backend service"
+
+
+
+
+
+
+
